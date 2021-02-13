3ae72b10b3dbc1a429d2bd373ad418d8b638ecd7
Anthony Ruiz <Ouroboratika@users.noreply.github.com>
2021-02-13 12:19:03 -0600

    Add initial logic for GET, POST, PUT, and DELETE commands and equations

--- api.py
+++ api.py
@@ -1,35 +1,100 @@
 import flask
-from flask import render_template
+from flask import redirect, render_template, url_for
+from flask_restful import Api, Resource, reqparse
 
 # Flask Configuration
 
 app = flask.Flask(__name__)
+api = Api(app)
 app.config["DEBUG"] = True
 
+# Config Vals
+
+MAX_EQNS = 10
+
+
+# App route configuration
+
 @app.route('/', methods=['GET'])
+@app.route('/index', methods=['GET'])
+def index():
+    return redirect(url_for("home"))
+
+@app.route('/home', methods=['GET'])
 def home():
     return render_template('home.html')
-
-@app.route('/api/v1/ops/add', methods=['POST'])
-def op_add():
-    pass
-
-@app.route('/api/v1/ops/subtract', methods=['POST'])
-def op_subtract():
-    pass
-
-@app.route('/api/v1/ops/multiply', methods=['POST'])
-def op_multiply():
-    pass
-
-@app.route('/api/v1/ops/divide', methods=['POST'])
-def op_divide():
-    pass
 
 @app.errorhandler(404)
 def page_not_found(e):
     return render_template('404_not_found.html'),404
 
-# Local Configuration
+# Classes
 
+class Eqn:
+    def __init__(self, a, op, b):
+        self.a = int(a)
+        self.op = op
+        self.b = int(b)
+        self.result = self.calc(self.a, self.op, self.b)
+
+    def calc(self, a, op, b):
+        if op == "a":
+            return a + b
+        elif op == "s":
+            return a - b
+        elif op == "d":
+            return a / b
+        elif op == "m":
+            return a * b
+
+    def to_dict(self):
+        return {"a":self.a, "op":self.op, "b":self.b, "result":self.result}
+
+class EqnRESTer(Resource):
+    def get(self):
+        return eqns, 200
+
+    def post(self):
+        parser = reqparse.RequestParser()
+        parser.add_argument("a")
+        parser.add_argument("op")
+        parser.add_argument("b")
+        args = parser.parse_args()
+
+        latest_eqn = Eqn(args["a"],args["op"],args["b"]).to_dict()
+
+        if(len(eqns) >= MAX_EQNS):
+            eqns.pop(0)
+        eqns.append(latest_eqn)
+
+        return latest_eqn, 201
+
+    def put(self):
+        parser = reqparse.RequestParser()
+        parser.add_argument("a")
+        parser.add_argument("op")
+        parser.add_argument("b")
+        args = parser.parse_args()
+
+        latest_eqn = Eqn(args["a"],args["op"],args["b"]).to_dict()
+
+        if len(eqns) > 0:
+            eqns[-1] = latest_eqn
+        else:
+            eqns.append(latest_eqn)
+        
+        return latest_eqn, 201
+
+    def delete(self):
+        global eqns
+        eqns = []
+        return "Equations list emptied", 200
+
+# Global vars
+
+eqns = []
+
+# Launch
+
+api.add_resource(EqnRESTer, "/eqn")
 app.run()
